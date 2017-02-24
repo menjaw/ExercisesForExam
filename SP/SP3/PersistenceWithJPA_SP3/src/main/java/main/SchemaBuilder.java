7fec42a3855b7f964dae0306e45176f7c1a71350
menjaw <menjaholm@gmail.com>
2017-02-24 11:00:33 +0100

    version 1 JPA entity mappings 3

---
+++ SP/SP3/PersistenceWithJPA_SP3/src/main/java/main/SchemaBuilder.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import javax.persistence.Persistence;
+
+/**
+ *
+ * @author Menja
+ */
+public class SchemaBuilder {
+
+    public static void main(String[] args) {
+        Persistence.generateSchema("PU", null);
+    }
+}
