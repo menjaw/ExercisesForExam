64b2e1fdd095cd3e411ceeaa582937265f8817c2
menjaw <menjaholm@gmail.com>
2017-03-01 20:24:00 +0100

    tried to add bootstrap - not working

--- Week5_Javascript/AjaxCallsJsExercise2_Day3/src/main/java/controller/PersonControl.java
+++ Week5_Javascript/AjaxCallsJsExercise2_Day3/src/main/java/controller/PersonControl.java
@@ -7,11 +7,14 @@ package controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import object.Person;
 
 /**
  *
@@ -37,7 +40,7 @@ public class PersonControl extends HttpServlet {
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet PersonControl</title>");            
+            out.println("<title>Servlet PersonControl</title>");
             out.println("</head>");
             out.println("<body>");
             out.println("<h1>Servlet PersonControl at " + request.getContextPath() + "</h1>");
@@ -72,6 +75,8 @@ public class PersonControl extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        List<Person> persons = new ArrayList();
+        
         processRequest(request, response);
     }
 
