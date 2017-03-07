2c5ae0cd4d1b866fa538f89737f4516f911db1d4
menjaw <menjaholm@gmail.com>
2017-03-07 20:54:01 +0100

    init commit

---
+++ Week6_REST/Rest_JAXRS_Client_AJAX_Day2/src/main/java/Interface/PersonFacade.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Interface;
+
+import entity.Person;
+import java.util.List;
+import javax.persistence.EntityManagerFactory;
+
+/**
+ *
+ * @author Menja
+ */
+public interface PersonFacade {
+    public void addEntityManagerFactory(EntityManagerFactory emf);
+    public Person addPerson(Person p);
+    public Person deletePerson(int id);
+    public Person getPerson(int id);
+    public List<Person> getPersons();
+    public Person editPerson(Person p);
+}
